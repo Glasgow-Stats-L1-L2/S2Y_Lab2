cfdc92ed21c3a553c8bcc69091f506f5cc36e53f
Glasgow-Stats-L1-L2 <111056800+Glasgow-Stats-L1-L2@users.noreply.github.com>
2023-01-23 18:14:06 +0000

    S2Y_Lab2

--- R_code.R
+++ R_code.R
@@ -10,19 +10,20 @@ physF <- subset(phys, Gender == "Female", data = phys)
 
 cor.test(physM$Power1, physM$Weight)
 
+### calculation by hand
+var(physM$Weight)
+var(physM$Power1)
+cov(physM$Weight,physM$Power1)
+cov(physM$Weight,physM$Power1) / sqrt(var(physM$Weight) * var(physM$Power1))
 
+M.mean.Weight <- mean(physM$Weight)
+M.mean.Power  <- mean(physM$Power1)
 
-#---------------------------------------------------------------------------------
-#---------------------------------------------------------------------------------
-# Nicholas Cage films and people drowning
+sum((physM$Weight - M.mean.Weight)^2)
+sum((physM$Power1 - M.mean.Power)^2)
+sum((physM$Weight - M.mean.Weight) * (physM$Power1 - M.mean.Power))
 
-Cage <- read.csv("Cage.csv")
-
-names(Cage)
-
-plot(NumDrowned ~ NumFilms, data = Cage, xlab = "Number of films",
-     ylab = "Number of people who drowned in pools", pch = 16)
-
-cor.test(Cage$NumDrowned, Cage$NumFilms)
-
-#---------------------------------------------------------------------------------
+Sxx <- sum((physM$Weight - M.mean.Weight)^2)
+Syy <- sum((physM$Power1 - M.mean.Power)^2)
+Sxy <- sum((physM$Weight - M.mean.Weight) * (physM$Power1 - M.mean.Power))
+Sxy / sqrt(Sxx * Syy)
